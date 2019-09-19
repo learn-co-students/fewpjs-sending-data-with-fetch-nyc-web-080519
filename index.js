8a90b3cb59039cf17407ec28c9eb0d0edd5f9732
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-09-18 21:14:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+const submitData = (name, email) => {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  })
+  .then(resp => resp.json())
+  .then(obj => {
+    document.body.append(obj["id"])
+  })
+  .catch(error => {
+    document.body.append(error.message);
+  })
+};
