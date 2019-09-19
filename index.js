e79be1efc2d1f09339d70bc1b3b21d103390f180
aresnik11 <aresnik11@gmail.com>
2019-09-18 22:06:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+    let data = {
+        name: name,
+        email: email
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            // console.log(json)
+            // console.log(json.id)
+            document.body.innerHTML += `<h1>${json.id}</h1>`
+        })
+        .catch(function(error) {
+            // console.log(error.message)
+            document.body.innerHTML += `<h1>${error.message}</h1>`
+        })
+}
